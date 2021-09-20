36b695a7773d5d4108b22a03eaa0bc4d15330a8f
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2021-09-19 23:16:50 -0300

    Corrigindo bug em que o botão era sobreposto pelo teclado

--- perguntados/lib/pages/login_page.dart
+++ perguntados/lib/pages/login_page.dart
@@ -10,6 +10,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Login'),
         centerTitle: true,
@@ -22,53 +23,69 @@ class LoginPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                    ),
-                  ),
+                  emailField(),
                   const SizedBox(
                     height: 20,
                   ),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Senha',
-                    ),
-                  ),
+                  passwordField(),
                   SizedBox(
                     height: 350,
                   ),
-                  Container(
-                      height: 48,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Color.fromRGBO(0, 0, 0, 0),
-                            onPrimary: defaultColor,
-                            shadowColor: Color.fromRGBO(0, 0, 0, 0),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24)),
-                            textStyle: TextStyle(fontSize: 20)),
-                        onPressed: () {},
-                        child: const Text('Cadastrar'),
-                      )),
+                  registerButton(),
                   SizedBox(height: 10),
-                  Container(
-                      height: 48,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: defaultColor,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(24)),
-                            textStyle: TextStyle(fontSize: 20)),
-                        onPressed: () {},
-                        child: const Text('Entrar'),
-                      ))
+                  loginButton()
                 ],
               ))),
+    );
+  }
+
+  Container registerButton() {
+    return Container(
+        height: 48,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Color.fromRGBO(0, 0, 0, 0),
+              onPrimary: defaultColor,
+              shadowColor: Color.fromRGBO(0, 0, 0, 0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24)),
+              textStyle: TextStyle(fontSize: 20)),
+          onPressed: () {},
+          child: const Text('Cadastrar'),
+        ));
+  }
+
+  Container loginButton() {
+    return Container(
+        height: 48,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: defaultColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24)),
+              textStyle: TextStyle(fontSize: 20)),
+          onPressed: () {},
+          child: const Text('Entrar'),
+        ));
+  }
+
+  TextField passwordField() {
+    return const TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Senha',
+      ),
+    );
+  }
+
+  TextField emailField() {
+    return const TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Email',
+      ),
     );
   }
 }
