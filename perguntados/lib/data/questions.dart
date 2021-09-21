13a7a1ed868478fc1a0108cf009c017938cf4b2c
Joao Paulo Prata Costa <joaoppcosta@hotmail.com.br>
2021-09-21 18:13:59 -0300

    Adicionando perguntas

--- perguntados/lib/data/questions.dart
+++ perguntados/lib/data/questions.dart
@@ -4,17 +4,41 @@ List<Question> allQuestions = [
   Question('Qual o nome do professor de flutter?',
       ['Gabriel Scobar', 'Gabriel Scotá', 'Pablo Scotá', 'Gabriel Esfolar'], 1),
   Question('Qual numero a baixo nao é primo?', ['1', '3', '4', '5'], 2),
-  Question('Qual o nome do professor de flutter?',
-      ['Gabriel Scobar', 'Gabriel Scotá', 'Pablo Scotá', 'Gabriel Esfolar'], 1),
-  Question('Qual numero a baixo nao é primo?', ['1', '3', '4', '5'], 2),
-  Question('Qual o nome do professor de flutter?',
-      ['Gabriel Scobar', 'Gabriel Scotá', 'Pablo Scotá', 'Gabriel Esfolar'], 1),
-  Question('Qual numero a baixo nao é primo?', ['1', '3', '4', '5'], 2),
-  Question('Qual o nome do professor de flutter?',
-      ['Gabriel Scobar', 'Gabriel Scotá', 'Pablo Scotá', 'Gabriel Esfolar'], 1),
-  Question('Qual numero a baixo nao é primo?', ['1', '3', '4', '5'], 2),
-  Question('Qual o nome do professor de flutter?',
-      ['Gabriel Scobar', 'Gabriel Scotá', 'Pablo Scotá', 'Gabriel Esfolar'], 1),
-  Question('Qual o nome do professor de flutter?',
-      ['Gabriel Scobar', 'Gabriel Scotá', 'Pablo Scotá', 'Gabriel Esfolar'], 1),
+  Question(
+      'Quem descobriu o Brasil?',
+      [
+        'Pedro Alvares Cabral',
+        'Michel Teló',
+        'Dilma Rouseff',
+        'DJ Cleyton Rasta'
+      ],
+      0),
+  Question('No xadrez, qual jogada define o fim da partida?',
+      ['Xeque Mate', 'Chá Mate', 'Não se mate', 'Iate'], 0),
+  Question('Qual o console de video game mais vendido do mundo?',
+      ['XBOX', 'PlayStation 3', 'Nintendo Switch', 'PlayStation 2'], 3),
+  Question('Qual time será campeão brasileiro em 2021?',
+      ['Flamento', 'Palmeiras', 'Atlético MG', 'Fluminense'], 2),
+  Question(
+      'O que significa a sigla UFOP?',
+      [
+        'Universidade Federal de Ouro Preto',
+        'Universidade Federal de Outro Planeta',
+        'Universidade Federal de Outro Plano',
+        'Universidade Federal de Ouro Prata'
+      ],
+      0),
+  Question('Qual das linguagens a baixo não é uma linguagem de programação?',
+      ['Java', 'Python', 'Lua', 'HTML'], 3),
+  Question('Qual a melhor trilha do Use Academy?',
+      ['Trilha iOS', 'Trilha Flutter', 'Trilha Design', 'Trilha Android'], 1),
+  Question(
+      'Qual o nome do jovem bilionário fundador do facebook?',
+      [
+        'Julio Cocielo',
+        'Arnold Schwarznegger ',
+        'Mark Zuckerberg',
+        'Gusttavo Lima'
+      ],
+      2),
 ];
